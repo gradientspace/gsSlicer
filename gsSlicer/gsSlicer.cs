751cd3c97d8b24bc73f320b3ca4495461e882efa
rms <rms@rms80.com>
2017-04-06 14:32:50 -0400

    add command-line tool

---
+++ gsSlicer/gsSlicer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using g3;
+
+namespace gsSlicer
+{
+    static class gsSlicer
+    {
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Polygon2d poly = new Polygon2d();
+            poly.AppendVertex(new Vector2d(0, 0));
+            poly.AppendVertex(new Vector2d(100, 0));
+            poly.AppendVertex(new Vector2d(100, 100));
+            poly.AppendVertex(new Vector2d(0, 1000));
+
+
+            
+
+        }
+
+    }
+}
