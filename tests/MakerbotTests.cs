37d902a0f9dae985b0632ebbf47acfb77f68432a
Ryan Schmidt <rms@rms80.com>
2017-06-04 14:01:16 -0400

    support experiments

--- tests/MakerbotTests.cs
+++ tests/MakerbotTests.cs
@@ -389,15 +389,17 @@ namespace gs
 			int nLayers = stack.Slices.Count;
 
 
-			int RoofFloorLayers = 2;
-			int InteriorSolidRegionContours = 1;
-			double InfillScale = 3.0;
-			double[] infill_angles = new double[] { -45, 45 };
+			settings.SparseLinearInfillStepX = 3.0;
 
+			// [TODO] move to settings...
+			int InteriorSolidRegionContours = 1;
+			double[] infill_angles = new double[] { -45, 45 };
+			bool AddSupport = true;
 
 			MakerbotCompiler cc = new MakerbotCompiler(builder, settings);
 
 			cc.Begin();
+			Vector3d CurrentPos = cc.NozzlePosition;
 
 
 			// compute shells for each layer
@@ -417,27 +419,83 @@ namespace gs
 			});
 
 
+			// should be parameterizable? this is 45 degrees...
+			//   (is it? 45 if nozzlediam == layerheight...)
+			//double fOverhangAllowance = 0.5 * settings.NozzleDiamMM;
+			double fOverhangAllowance = settings.LayerHeightMM / Math.Tan(45 * MathUtil.Deg2Rad);
 
+			List<GeneralPolygon2d>[] LayerSupportAreas = new List<GeneralPolygon2d>[nLayers];
+			if (AddSupport) {
+
+				// Compute required support area for each layer
+				// Note that this does *not* include thickness allowance, this is
+				// the "outer" support-requiring polygon
+				gParallel.ForEach(Interval1i.Range(nLayers - 1), (layeri) => {
+					PlanarSlice slice = stack.Slices[layeri];
+					PlanarSlice next_slice = stack.Slices[layeri + 1];
+
+					List<GeneralPolygon2d> insetPolys = ClipperUtil.MiterOffset(next_slice.Solids, -fOverhangAllowance);
+					List<GeneralPolygon2d> supportPolys = ClipperUtil.Difference(insetPolys, slice.Solids);
+					LayerSupportAreas[layeri] = supportPolys;
+				});
+				LayerSupportAreas[nLayers - 1] = new List<GeneralPolygon2d>();
+
+				// now merge support layers. Process is to track "current" support area,
+				// at layer below we union with that layers support, and then subtract
+				// that layers solids. 
+				List<GeneralPolygon2d> prevSupport = LayerSupportAreas[nLayers - 1];
+				for (int i = nLayers - 2; i >= 0; --i) {
+					PlanarSlice slice = stack.Slices[i];
+
+
+					// union down
+					List<GeneralPolygon2d> combineSupport = null;
+					bool dilate = true;
+					double fDilate = settings.NozzleDiamMM * 0.5;
+					if (dilate) {
+						List<GeneralPolygon2d> a = ClipperUtil.MiterOffset(prevSupport, fDilate);
+						List<GeneralPolygon2d> b = ClipperUtil.MiterOffset(LayerSupportAreas[i], fDilate);
+						combineSupport = ClipperUtil.Union(a, b);
+						combineSupport = ClipperUtil.MiterOffset(combineSupport, -fDilate);
+					} else {
+						combineSupport = ClipperUtil.Union(prevSupport, LayerSupportAreas[i]);
+					}
+
+					// support area we propagate down is combined area minus solid
+					prevSupport = ClipperUtil.Difference(combineSupport, slice.Solids);
+
+					// on this layer, we need to leave space for filament, by dilating solid by
+					// half nozzle-width and subtracting it
+					List<GeneralPolygon2d> dilatedSolid = ClipperUtil.MiterOffset(slice.Solids, settings.NozzleDiamMM * 0.5);
+					combineSupport = ClipperUtil.Difference(combineSupport, dilatedSolid);
+
+					// the actual area we will support is nudged inwards
+					//LayerSupportAreas[i] = ClipperUtil.MiterOffset(combineSupport, -fOverhangAllowance);
+					LayerSupportAreas[i] = DeepCopy.List(combineSupport);
+				}
+			} else {
+				for (int i = 0; i < nLayers; ++i)
+					LayerSupportAreas[i] = new List<GeneralPolygon2d>();
+			}
+
+
+			// generate paths for print area
 			for (int i = 0; i < nLayers; ++i) {
 				System.Console.WriteLine("Layer {0} of {1}", i, nLayers);
 
 				PlanarSlice slice = stack.Slices[i];
 
 				PathSetBuilder paths = new PathSetBuilder();
-				paths.Initialize(cc.NozzlePosition);
+				paths.Initialize(CurrentPos);
 				Vector3d currentPos = paths.Position;
 
 				// layer-up
 				currentPos = paths.AppendZChange(settings.LayerHeightMM, settings.ZTravelSpeed);
 
-				bool is_infill = (i >= RoofFloorLayers && i < nLayers - RoofFloorLayers - 1);
-				double fillScale = (is_infill) ? InfillScale : 1.0f;
+				bool is_infill = (i >= settings.FloorLayers && i < nLayers - settings.RoofLayers - 1);
+				double fillScale = (is_infill) ? settings.SparseLinearInfillStepX : 1.0f;
 
 				PathScheduler scheduler = new PathScheduler(paths, settings);
-
-				// should be parameterizable? this is 45 degrees...
-				//   (is it? 45 if nozzlediam == layerheight...)
-				double fOverhangAllowance = 0.5 * settings.NozzleDiamMM;
 
 				// construct region that needs to be solid for "roofs".
 				// This is the intersection of infill polygons for the next N layers
@@ -449,7 +507,7 @@ namespace gs
 					// If we want > 1 roof layer, we need to look further ahead.
 					// The full area we need to print as "roof" is the infill minus
 					// the intersection of the infill areas above
-					for (int k = 2; k <= RoofFloorLayers; ++k) {
+					for (int k = 2; k <= settings.RoofLayers; ++k) {
 						int ri = i + k;
 						if (ri < LayerShells.Length) {
 							List<GeneralPolygon2d> infillN = new List<GeneralPolygon2d>();
@@ -475,7 +533,7 @@ namespace gs
 						floor_cover.AddRange(shells.InnerPolygons);
 
 					// If we want > 1 floor layer, we need to look further back.
-					for (int k = 2; k <= RoofFloorLayers; ++k) {
+					for (int k = 2; k <= settings.FloorLayers; ++k) {
 						int ri = i - k;
 						if (ri > 0) {
 							List<GeneralPolygon2d> infillN = new List<GeneralPolygon2d>();
@@ -561,7 +619,7 @@ namespace gs
 					foreach (GeneralPolygon2d infill_poly in infillPolys) {
 						DenseLinesFillPolygon infill_gen = new DenseLinesFillPolygon(infill_poly) {
 							InsetFromInputPolygon = false,
-							PathSpacing = InfillScale * settings.FillPathSpacingMM,
+							PathSpacing = fillScale * settings.FillPathSpacingMM,
 							ToolWidth = settings.NozzleDiamMM,
 							AngleDeg = infill_angles[i % infill_angles.Length]
 						};
@@ -570,9 +628,34 @@ namespace gs
 					}
 				}
 
-				cc.AppendPaths(paths.Paths);
-			}
 
+				// fill support areas
+				foreach (GeneralPolygon2d area in LayerSupportAreas[i]) {
+
+					//DenseLinesFillPolygon support_gen = new DenseLinesFillPolygon(area) {
+					//	InsetFromInputPolygon = false,
+					//	PathSpacing = 3 * settings.FillPathSpacingMM,
+					//	ToolWidth = settings.NozzleDiamMM,
+					//	AngleDeg = 90
+					//};
+					//support_gen.Compute();
+					//scheduler.Append(support_gen.Paths);
+
+
+					ShellsFillPolygon shells_gen = new ShellsFillPolygon(area);
+					shells_gen.PathSpacing = settings.FillPathSpacingMM;
+					shells_gen.ToolWidth = settings.NozzleDiamMM;
+					shells_gen.InsetFromInputPolygon = false;
+					shells_gen.Layers = 1;
+					shells_gen.Compute();
+					scheduler.AppendShells(shells_gen.Shells);		
+				}
+
+
+				cc.AppendPaths(paths.Paths);
+				CurrentPos = cc.NozzlePosition;
+			}
+			
 			cc.End();
 
 			System.Console.WriteLine("[MakerbotTests] total extrude length: {0}", cc.ExtruderA);
